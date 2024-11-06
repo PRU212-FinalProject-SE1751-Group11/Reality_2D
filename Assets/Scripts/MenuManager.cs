cedf2a768bf631455aeb8b244e8fc9e97e81df62
hungngooo <153660237+hungngooo@users.noreply.github.com>
2024-11-07 00:05:56 +0700

    Add About Us Scene

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -12,11 +12,18 @@ public class MenuManager : MonoBehaviour
         // Tìm button và gán sự kiện click
         Button instructionButton = GameObject.Find("Button Instruction").GetComponent<Button>();
         instructionButton.onClick.AddListener(OnInstructionButtonClick);
+        Button aboutUsButton = GameObject.Find("Button Aboutus").GetComponent<Button>();
+        aboutUsButton.onClick.AddListener(OnAboutUsButtonClick);
     }
 
     void OnInstructionButtonClick()
     {
         // Chuyển đổi scene
         SceneManager.LoadScene("Tutorial");
+    }
+    void OnAboutUsButtonClick()
+    {
+        // Chuyển đổi scene
+        SceneManager.LoadScene("AboutUs");
     }
 }
