4d7ca53b2f93dc384a74be3db100706dd5ae537c
jendly365 <trong1met8@gmail.com>
2024-11-07 11:36:26 +0700

    done

---
+++ Assets/Scripts/MatureManage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatureManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float waitTime = 5f;     // Thời gian chờ trước khi fade
+    public float fadeDuration = 1f; // Thời gian để fade
+
+    void Start()
+    {
+        StartCoroutine(WaitAndLoadScene());
+    }
+
+    private IEnumerator WaitAndLoadScene()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        Initiate.Fade("Thanksplaying", Color.black, 0.3f);
+    }
+}
